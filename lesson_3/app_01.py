8bb2193d397ddab4b3485f3b10174fa03f50a0b8
Alevtin_linux_home <antonov3017@gmail.com>
2023-08-12 20:18:02 +0300

    l_3 init

---
+++ lesson_3/app_01.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
+
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] =
+# 'mysql+pymysql://username:password@hostname/database_name'
+# db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return 'Hi!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
